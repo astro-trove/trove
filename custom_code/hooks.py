c1c46bce4592766ba28cc7e01eaf63d394d092f4
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2022-04-28 16:14:04 -0700

    pass coord list to static_cats_query

--- custom_code/hooks.py
+++ custom_code/hooks.py
@@ -9,7 +9,7 @@ def target_post_save(target, created):
     """This hook runs following update of a target."""
     logger.info('Target post save hook: %s created: %s', target, created)
 
-    qprob, qso, qoffset = static_cats_query(target.ra, target.dec)
+    qprob, qso, qoffset = static_cats_query([target.ra], [target.dec])
     TargetExtra(target=target, key='qprob', value=qprob).save()
     TargetExtra(target=target, key='qso', value=qso).save()
     TargetExtra(target=target, key='qoffset', value=qoffset).save()
