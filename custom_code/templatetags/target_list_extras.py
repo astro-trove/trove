0d92999a5ce486969c60593ae4fe2559b54c3073
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2022-07-25 11:51:48 -0700

    simplify galaxy table inclusion tag

--- custom_code/templatetags/target_list_extras.py
+++ custom_code/templatetags/target_list_extras.py
@@ -1,8 +1,6 @@
 from django import template
 from ..models import TargetListExtra
-from django.conf import settings
 from tom_targets.models import TargetExtra
-from django.utils.safestring import mark_safe
 import json
 
 register = template.Library()
@@ -18,19 +16,15 @@ def target_list_extra_field(target_list, name):
     except TargetListExtra.DoesNotExist:
         return None
 
+
 @register.inclusion_tag('tom_targets/partials/galaxy_table.html')
 def galaxy_table(target):
     """
     Displays the most likely host galaxy matches.
     """
-    extras = {k['name']: target.extra_fields.get(k['name'], '') for k in settings.EXTRA_FIELDS if not k.get('hidden')}
-    return {
-        'target': target,
-        'extras': extras
-    }
-
-@register.filter
-def get_host_galaxies(target):
     te = TargetExtra.objects.filter(target=target, key='Host Galaxies')
     if te.exists():
-        return json.loads(te.first().value)
+        galaxies = json.loads(te.first().value)
+    else:
+        galaxies = None
+    return {'galaxies': galaxies}
