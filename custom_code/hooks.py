333ca0c0d3e8dc757dd92e21aa73466caffe7822
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2022-07-25 11:50:49 -0700

    clean up custom_code/hooks.py

--- custom_code/hooks.py
+++ custom_code/hooks.py
@@ -13,16 +13,13 @@ def target_post_save(target, created):
 
     qprob, qso, qoffset, asassnprob, asassn, asassnoffset = static_cats_query([target.ra], [target.dec])
     TargetExtra(target=target, key='QSO Match', value=qso[0]).save()
-    if qso[0]!='None':
+    if qso[0] != 'None':
         TargetExtra(target=target, key='QSO Prob.', value=qprob[0]).save()
         TargetExtra(target=target, key='QSO Offset', value=qoffset[0]).save()
     TargetExtra(target=target, key='ASASSN Match', value=asassn[0]).save()
-    if asassn[0]!='None':
+    if asassn[0] != 'None':
         TargetExtra(target=target, key='ASASSN Prob.', value=asassnprob[0]).save()
         TargetExtra(target=target, key='ASASSN Offset', value=asassnoffset[0]).save()
 
     matches, hostdict = galaxy_search([target.ra], [target.dec])
-    print(matches)
     TargetExtra(target=target, key='Host Galaxies', value=json.dumps(hostdict)).save()
-    # else:
-    #     TargetExtra(target=target, key='Best Host Pcc', value='None').save()
