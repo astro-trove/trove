4313d1c0c9a1d3c0c1a296308f5dc41cfac6a251
Phil Daly <pndaly@arizona.edu>
2022-08-10 14:42:55 -0700

    Correct path to settings_local.py

--- saguaro_tom/settings.py
+++ saguaro_tom/settings.py
@@ -11,7 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-from settings_local import *
+from saguaro_tom.settings_local import *
 
 import logging.config
 import os
