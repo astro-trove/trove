633ad8280417612904fdaa85739b7cf1d7001717
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2022-08-12 11:37:46 -0700

    clean up recent photometry

--- custom_code/templatetags/photometry_extras.py
+++ custom_code/templatetags/photometry_extras.py
@@ -1,42 +1,29 @@
-from urllib.parse import urlencode
-
 from django import template
 from django.conf import settings
-from django.contrib.auth.models import Group
-from django.core.paginator import Paginator
-from django.shortcuts import reverse
-from django.utils import timezone
-from datetime import datetime, timedelta
 from guardian.shortcuts import get_objects_for_user
 from plotly import offline
 import plotly.graph_objs as go
-from io import BytesIO
-from PIL import Image, ImageDraw
-import base64
-
-from tom_dataproducts.forms import DataProductUploadForm
-from tom_dataproducts.models import DataProduct, ReducedDatum
-from tom_observations.models import ObservationRecord
-from tom_targets.models import Target
+from tom_dataproducts.models import ReducedDatum
 
 register = template.Library()
 
-@register.inclusion_tag('tom_dataproducts/partials/recent_photometry.html')
-def recent_photometry(target, limit=1):
+
+@register.inclusion_tag('tom_dataproducts/partials/recent_photometry.html', takes_context=True)
+def recent_photometry(context, target, limit=1):
     """
     Displays a table of the most recent photometric points for a target.
     """
 
     if settings.TARGET_PERMISSIONS_ONLY:
-        datums = ReducedDatum.objects.filter(target=target, data_type=settings.DATA_PRODUCT_TYPES['photometry'][0]).order_by('-timestamp')[:limit]
+        datums = ReducedDatum.objects.filter(target=target, data_type=settings.DATA_PRODUCT_TYPES['photometry'][0])
     else:
         datums = get_objects_for_user(context['request'].user,
                                       'tom_dataproducts.view_reduceddatum',
                                       klass=ReducedDatum.objects.filter(
                                         target=target,
-                                        data_type=settings.DATA_PRODUCT_TYPES['photometry'][0])).order_by('-timestamp')[:limit]
+                                        data_type=settings.DATA_PRODUCT_TYPES['photometry'][0]))
     recent_det = {'data': []}
-    for datum in datums:
+    for datum in datums.order_by('-timestamp')[:limit]:
         if 'magnitude' in datum.value.keys():
             recent_det['data'].append({'timestamp': datum.timestamp, 'magnitude': datum.value['magnitude']})
         elif 'limit' in datum.value.keys():
@@ -44,15 +31,7 @@ def recent_photometry(target, limit=1):
         else:
             continue
 
-    if len(recent_det['data'])>0:
-        return recent_det
-    else:
-        return
-
-    # if magnitude in photometry.keys():
-    #     return {'data': [{'timestamp': rd.timestamp, 'magnitude': rd.value['magnitude']} for rd in photometry]}
-    # else:
-    #     return
+    return recent_det
 
 
 @register.inclusion_tag('tom_dataproducts/partials/photometry_for_target.html', takes_context=True)
