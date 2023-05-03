a58ab1c800189d3b1a136d8c0edffe0fda9cf145
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-05-02 17:31:09 -0700

    remove fill-in-the-blanks from notes

--- custom_code/facilities.py
+++ custom_code/facilities.py
@@ -21,6 +21,14 @@ class CustomBinospecSpectroscopyForm(CustomMMTObservationForm, MMTSpectroscopyFo
 
 
 class CustomMMIRSObservationForm(CustomMMTObservationForm):
+    def __init__(self, *args, **kwargs):
+        kwargs['initial']['notes'] = 'Please use a random 30" dither pattern (4 exposures per position if K-band). ' \
+                                     'Please guide for individual exposures. I have put in a dither size of 30" but ' \
+                                     'this just specifies my estimated box size to not lose guiding (not the size of ' \
+                                     'the individual dithers). We do not care about the position angle so adjust as ' \
+                                     'needed for the guide star.'
+        super().__init__(*args, **kwargs)
+
     def layout(self):
         return Layout(
             HTML('<p><a href="https://docs.google.com/document/d/1-ZKvIQxH7LRqWM5hAyqlVBLhtnncGlCYYHWzkghN-jk/edit">'
@@ -30,24 +38,11 @@ class CustomMMIRSObservationForm(CustomMMTObservationForm):
 
 
 class CustomMMIRSImagingForm(CustomMMIRSObservationForm, MMTMMIRSImagingForm):
-    def __init__(self, *args, **kwargs):
-        kwargs['initial']['notes'] = 'Imaging of a GW follow-up with expected {FILL IN FILTER} ~ {FILL IN MAG} mag. ' \
-                                     'Please use a random 30" dither pattern (4 exposures per position if K-band). ' \
-                                     'Please guide for individual exposures. I have put in a dither size of 30" but ' \
-                                     'this just specifies my estimated box size to not lose guiding (not the size of ' \
-                                     'the individual dithers). We do not care about the position angle so adjust as ' \
-                                     'needed for the guide star.'
-        super().__init__(*args, **kwargs)
+    pass
 
 
 class CustomMMIRSSpectroscopyForm(CustomMMIRSObservationForm, MMTMMIRSSpectroscopyForm):
-    def __init__(self, *args, **kwargs):
-        kwargs['initial']['notes'] = 'Please use a random 30" dither pattern. Please guide for individual exposures. ' \
-                                     'I have put in a dither size of 30" but this just specifies my estimated box ' \
-                                     'size to not lose guiding (not the size of the individual dithers). We do not ' \
-                                     'care about the position angle so adjust as needed for the guide star.'
-        super().__init__(*args, **kwargs)
-
+    pass
 
 class CustomMMTFacility(MMTFacility):
     observation_forms = {
