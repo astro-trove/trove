01d43bc2e418d81bccb8b9750d4d7587079dfeba
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-05-05 18:33:14 -0700

    add GW Events fields to Candidates filters

--- custom_code/filters.py
+++ custom_code/filters.py
@@ -1,6 +1,12 @@
 import django_filters
-from .models import Candidate
+from .models import CSSField
+import json
+from django.conf import settings
+from astropy.time import Time
 from tom_targets.utils import cone_search_filter
+from tom_nonlocalizedevents.models import NonLocalizedEvent
+
+CREDIBLE_REGION_PERCENTS = [int(100 * p) for p in json.loads(settings.CREDIBLE_REGION_PROBABILITIES)]
 
 
 class CandidateFilter(django_filters.FilterSet):
@@ -22,12 +28,32 @@ class CandidateFilter(django_filters.FilterSet):
 
         return cone_search_filter(queryset, ra, dec, radius)
 
+    field = django_filters.ModelChoiceFilter(queryset=CSSField.objects)
+    classification = django_filters.ChoiceFilter(choices=[(0, 'Transient'), (1, 'Moving Object')])
     snr_min = django_filters.NumberFilter('snr', 'gte', label='Min. S/N')
     mag_range = django_filters.NumericRangeFilter('mag', label='Magnitude')
     obsdate_range = django_filters.DateTimeFromToRangeFilter('obsdate', label='Obs. Date')
     mlscore_range = django_filters.NumericRangeFilter('mlscore', 'gte', label='ML Old')
     mlscore_real_range = django_filters.NumericRangeFilter('mlscore_real', label='ML Real')
     mlscore_bogus_range = django_filters.NumericRangeFilter('mlscore_bogus', label='ML Bogus')
+
+    def nle_filter(self, queryset, name, value):
+        if name == 'nonlocalizedevent':
+            seq = value.sequences.last()
+            mjd = Time(seq.details['time']).mjd
+            queryset = queryset.filter(mjdmid__gte=mjd, mjdmid__lte=mjd + 3.,
+                                       field__css_field_credible_regions__localization=seq.localization)
+        return queryset
+
+    def prob_filter(self, queryset, name, value):
+        if name == 'probability':
+            queryset = queryset.filter(field__css_field_credible_regions__smallest_percent__lte=value)
+        return queryset
+
+    nonlocalizedevent = django_filters.ModelChoiceFilter(queryset=NonLocalizedEvent.objects.order_by('-created'),
+                                                         method='nle_filter', label='GW Event')
+    probability = django_filters.ChoiceFilter(choices=[(p, p) for p in CREDIBLE_REGION_PERCENTS][::-1],
+                                              method='prob_filter', label='Credible Region', empty_label=100)
 
     order = django_filters.OrderingFilter(
         fields=['obsdate', 'ra', 'dec', 'snr', 'mag', 'mlscore', 'mlscore_real', 'mlscore_bogus'],
@@ -43,7 +69,3 @@ class CandidateFilter(django_filters.FilterSet):
         }
     )
 
-    class Meta:
-        model = Candidate
-        fields = ['cone_search', 'snr_min', 'mag_range', 'obsdate_range', 'field', 'classification',
-                  'mlscore_range', 'mlscore_real_range', 'mlscore_bogus_range']
