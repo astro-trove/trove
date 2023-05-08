29eb070e2c795666ebde9ee4d3d9c09686b7da2d
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-05-07 21:53:46 -0700

    create combined localization filter for candidates

--- custom_code/filters.py
+++ custom_code/filters.py
@@ -1,12 +1,63 @@
+import django.forms
 import django_filters
 from .models import CSSField
 import json
 from django.conf import settings
-from astropy.time import Time
+from datetime import datetime, timedelta
 from tom_targets.utils import cone_search_filter
 from tom_nonlocalizedevents.models import NonLocalizedEvent
 
-CREDIBLE_REGION_PERCENTS = [int(100 * p) for p in json.loads(settings.CREDIBLE_REGION_PROBABILITIES)]
+CREDIBLE_REGION_PROBABILITIES = json.loads(settings.CREDIBLE_REGION_PROBABILITIES)
+CREDIBLE_REGION_CHOICES = [(int(100. * p), f'{p:.0%}') for p in CREDIBLE_REGION_PROBABILITIES]
+
+
+def _get_nonlocalized_event_choices():
+    return [(None, '-------')] + [(nle, str(nle)) for nle in NonLocalizedEvent.objects.order_by('-created')]
+
+
+class LocalizationWidget(django.forms.widgets.MultiWidget):
+    def __init__(self, **kwargs):
+        widgets = {
+            'event': django.forms.Select(choices=_get_nonlocalized_event_choices()),
+            'prob': django.forms.Select(choices=CREDIBLE_REGION_CHOICES),
+            'dt': django.forms.NumberInput(attrs={'placeholder': 'days after'}),
+        }
+        super().__init__(widgets, **kwargs)
+
+    def decompress(self, value):
+        return value
+
+
+class LocalizationField(django.forms.MultiValueField):
+    def __init__(self, **kwargs):
+        fields = (
+            django.forms.TypedChoiceField(choices=_get_nonlocalized_event_choices(),
+                                          coerce=lambda name: NonLocalizedEvent.objects.get(event_id=name)),
+            django.forms.TypedChoiceField(choices=CREDIBLE_REGION_CHOICES, coerce=int),
+            django.forms.FloatField(min_value=0., initial=3.)
+        )
+        super().__init__(fields, widget=LocalizationWidget, **kwargs)
+
+    def compress(self, data_list):
+        return data_list
+
+
+class LocalizationFilter(django_filters.Filter):
+    field_class = LocalizationField
+
+    def filter(self, queryset, value):
+        if value and value[1]:
+            nle, prob, dt = value
+            seq = nle.sequences.last()
+            if seq is None or seq.details is None:
+                return queryset.none()
+            tmin = datetime.strptime(seq.details['time'], '%Y-%m-%dT%H:%M:%S.%f%z')
+            tmax = datetime.now(tmin.tzinfo) if dt is None else tmin + timedelta(days=dt)
+            return queryset.filter(field__css_field_credible_regions__localization=seq.localization,
+                                   field__css_field_credible_regions__smallest_percent__lte=prob,
+                                   obsdate__gte=tmin, obsdate__lte=tmax)
+        else:
+            return queryset
 
 
 class CandidateFilter(django_filters.FilterSet):
@@ -36,24 +87,7 @@ class CandidateFilter(django_filters.FilterSet):
     mlscore_range = django_filters.NumericRangeFilter('mlscore', 'gte', label='ML Old')
     mlscore_real_range = django_filters.NumericRangeFilter('mlscore_real', label='ML Real')
     mlscore_bogus_range = django_filters.NumericRangeFilter('mlscore_bogus', label='ML Bogus')
-
-    def nle_filter(self, queryset, name, value):
-        if name == 'nonlocalizedevent':
-            seq = value.sequences.last()
-            mjd = Time(seq.details['time']).mjd
-            queryset = queryset.filter(mjdmid__gte=mjd, mjdmid__lte=mjd + 3.,
-                                       field__css_field_credible_regions__localization=seq.localization)
-        return queryset
-
-    def prob_filter(self, queryset, name, value):
-        if name == 'probability':
-            queryset = queryset.filter(field__css_field_credible_regions__smallest_percent__lte=value)
-        return queryset
-
-    nonlocalizedevent = django_filters.ModelChoiceFilter(queryset=NonLocalizedEvent.objects.order_by('-created'),
-                                                         method='nle_filter', label='GW Event')
-    probability = django_filters.ChoiceFilter(choices=[(p, p) for p in CREDIBLE_REGION_PERCENTS][::-1],
-                                              method='prob_filter', label='Credible Region', empty_label=100)
+    localization = LocalizationFilter(label='Localization')
 
     order = django_filters.OrderingFilter(
         fields=['obsdate', 'ra', 'dec', 'snr', 'mag', 'mlscore', 'mlscore_real', 'mlscore_bogus'],
