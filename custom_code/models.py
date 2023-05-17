810ca4b74f0fe53f831281eecf3fb50336cbf577
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-05-17 02:39:49 -0700

    credible region contours are unique

--- custom_code/models.py
+++ custom_code/models.py
@@ -180,3 +180,8 @@ class CredibleRegionContour(models.Model):
     localization = models.ForeignKey(EventLocalization, related_name='credible_region_contours', on_delete=models.CASCADE)
     probability = models.FloatField()
     pixels = models.JSONField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['localization', 'probability'], name='unique_localization_probability')
+        ]
