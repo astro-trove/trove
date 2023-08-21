d10b4a74223f7e420f0c56e0db7431c8dbdd1a1f
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-08-21 15:50:51 -0400

    directly point to thumbnail URLs

--- custom_code/templatetags/candidate_extras.py
+++ custom_code/templatetags/candidate_extras.py
@@ -13,9 +13,11 @@ def thumbnail_url(candidate, suffix):
     visit = candidate.observation_record.observation_id.split('_')[4]
     url = f'http://sassy.as.arizona.edu/papp/api/{candidate.observation_record.scheduled_start.strftime("%Y/%m/%d")}/'
     url += f'{candidate.observation_record.survey_field}/{candidate.candidatenumber}_{visit}_{suffix}.png'
-    try:
-        response = requests.get(url, timeout=0.2)
-        return 'data:image/png;base64,' + base64.b64encode(response.content).decode() if response.ok else ''
-    except Exception as e:
-        logger.error(f'Could not reach url {url}: {e}')
-        return ''
+    return url
+    # use the following if the URL is not public
+    # try:
+    #     response = requests.get(url, timeout=0.2)
+    #     return 'data:image/png;base64,' + base64.b64encode(response.content).decode() if response.ok else ''
+    # except Exception as e:
+    #     logger.error(f'Could not reach url {url}: {e}')
+    #     return ''
