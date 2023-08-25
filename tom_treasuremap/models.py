82544242518b6f7c0a47b503a8f6012d7cb74ec7
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-08-25 12:20:30 -0400

    allow for separate planned and completed treasure map pointings

--- tom_treasuremap/models.py
+++ tom_treasuremap/models.py
@@ -4,7 +4,7 @@ from tom_surveys.models import SurveyObservationRecord
 
 
 class TreasureMapPointing(models.Model):
-    treasuremap_id = models.IntegerField(null=True, unique=True)
+    treasuremap_id = models.IntegerField(unique=True)
     nonlocalizedevent = models.ForeignKey(NonLocalizedEvent, related_name='treasuremap_pointings',
                                           on_delete=models.CASCADE)
     observation_record = models.ForeignKey(SurveyObservationRecord, related_name='treasuremap_pointings',
@@ -14,8 +14,8 @@ class TreasureMapPointing(models.Model):
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['nonlocalizedevent', 'observation_record'],
-                name='unique_nonlocalizedevent_observation_record',
+                fields=['nonlocalizedevent', 'observation_record', 'status'],
+                name='unique_nonlocalizedevent_observation_record_status',
             )
         ]
 
--- tom_treasuremap/reporting.py
+++ tom_treasuremap/reporting.py
@@ -10,35 +10,25 @@ logger = logging.getLogger(__name__)
 
 
 def report_to_treasure_map(observation_records, nle):
-    treasuremap_pointings = []
-    for sor in observation_records:
-        tmps = TreasureMapPointing.objects.filter(nonlocalizedevent=nle, observation_record=sor)
-        if tmps.exists():
-            tmp = tmps.first()
-            if tmp.status == 'PENDING' and sor.status == 'COMPLETED':  # update planned pointing to completed
-                tmp.status = sor.status
-            else:  # this should never happen
-                logger.error(f'There is already a {tmp.status} pointing for {sor.status} {sor}. Not reporting.')
-                continue
-        else:
-            tmp = TreasureMapPointing(nonlocalizedevent=nle, observation_record=sor, status=sor.status)
-        treasuremap_pointings.append(tmp)  # don't save them to the db until we know the submission was successful
-
+    new_observations = []
     json_pointings = []
-    for tmp in treasuremap_pointings:
+    for sor in observation_records:
+        tmps = TreasureMapPointing.objects.filter(nonlocalizedevent=nle, observation_record=sor, status=sor.status)
+        if tmps.exists():
+            logger.info(f'{sor} has already been reported as {sor.status}.')
+            continue
         pointing = {
-            'ra': tmp.observation_record.survey_field.ra,
-            'dec': tmp.observation_record.survey_field.dec,
+            'ra': sor.survey_field.ra,
+            'dec': sor.survey_field.dec,
             'pos_angle': 0.,  # survey fields have a fixed position angle
-            'instrumentid': TREASUREMAP_INSTRUMENT_IDS.get(tmp.observation_record.facility),
-            'time': tmp.observation_record.scheduled_start.strftime('%Y-%m-%dT%H:%M:%S'),
-            'status': 'planned' if tmp.status == 'PENDING' else tmp.status.lower(),  # convert to TM terminology
+            'instrumentid': TREASUREMAP_INSTRUMENT_IDS.get(sor.facility),
+            'time': sor.scheduled_start.strftime('%Y-%m-%dT%H:%M:%S'),
+            'status': 'planned' if sor.status == 'PENDING' else sor.status.lower(),  # convert to TM terminology
             'depth': 21.5,
             'depth_unit': 'ab_mag',
             'band': 'open',
         }
-        if tmp.treasuremap_id is not None:
-            pointing['id'] = tmp.treasuremap_id
+        new_observations.append(sor)
         json_pointings.append(pointing)
 
     json_data = {'api_token': settings.TREASUREMAP_API_KEY, 'graceid': nle.event_id, 'pointings': json_pointings}
@@ -46,10 +36,10 @@ def report_to_treasure_map(observation_records, nle):
     if response.ok:
         response_json = response.json()
         submitted_pointings = response_json['pointing_ids']
-        for tmp, treasuremap_id in zip(treasuremap_pointings, submitted_pointings):
-            tmp.treasuremap_id = treasuremap_id
-            tmp.save()
-        success = f'Submitted {len(treasuremap_pointings):d} pointings for {nle.event_id} to the Treasure Map'
+        for sor, treasuremap_id in zip(new_observations, submitted_pointings):
+            TreasureMapPointing.objects.create(treasuremap_id=treasuremap_id, nonlocalizedevent=nle,
+                                               observation_record=sor, status=sor.status)
+        success = f'Submitted {len(submitted_pointings):d} pointings for {nle.event_id} to the Treasure Map'
         logger.info(success)
         response_json['SUCCESSES'] = [success]
         for error in response_json['ERRORS']:
