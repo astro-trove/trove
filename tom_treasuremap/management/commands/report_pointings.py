2e971de549c385dc725e06ad37a0fb7596f9cb63
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-08-25 10:39:36 -0400

    avoid time zone warning in reporting to treasure map

--- tom_treasuremap/management/commands/report_pointings.py
+++ tom_treasuremap/management/commands/report_pointings.py
@@ -3,7 +3,7 @@ from tom_nonlocalizedevents.models import NonLocalizedEvent
 from tom_surveys.models import SurveyObservationRecord
 from ...reporting import report_to_treasure_map
 from custom_code.filters import LocalizationFilter
-from astropy.time import Time
+from astropy.time import Time, TimezoneInfo
 from astropy import units as u
 import logging
 
@@ -37,8 +37,8 @@ class Command(BaseCommand):
         logger.info(f'Found active nonlocalized events: {", ".join([nle.event_id for nle in active_nles])}')
 
         lookback_window_obs = now - lookback_days_obs * u.day
-        recent_obs = SurveyObservationRecord.objects.filter(created__gte=lookback_window_obs.datetime,
-                                                            created__lt=now.datetime)
+        recent_obs = SurveyObservationRecord.objects.filter(created__gt=lookback_window_obs.to_datetime(TimezoneInfo()),
+                                                            created__lte=now.to_datetime(TimezoneInfo()))
         loc_filter = LocalizationFilter()
         for nle in active_nles:
             matching_observations = loc_filter.filter(recent_obs, (nle, contour_percent, lookback_days_nle))
