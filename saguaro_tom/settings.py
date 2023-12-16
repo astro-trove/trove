9f89bd103889394ffe557c68f9083d700b52e902
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2023-12-15 17:07:44 -0700

    avoid unknown data product types

--- saguaro_tom/settings.py
+++ saguaro_tom/settings.py
@@ -249,7 +249,7 @@ DATA_PRODUCT_TYPES = {
 DATA_PROCESSORS = {
     'photometry': 'tom_dataproducts.processors.photometry_processor.PhotometryProcessor',
     'spectroscopy': 'custom_code.processors.spectroscopy_processor.SpectroscopyProcessor',
-    '1D FITS Tar': 'tom_mmt.mmt.MMTDataProcessor',
+    'MMT': 'tom_mmt.mmt.MMTDataProcessor',
 }
 
 TOM_FACILITY_CLASSES = [
