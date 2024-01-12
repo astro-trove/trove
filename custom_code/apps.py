f16de60ec465d0f1dcb5d990cc07868253254711
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2024-01-12 15:02:00 -0700

    switch vetting button to new get_buttons mechanism

--- custom_code/apps.py
+++ custom_code/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CustomCodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'custom_code'
+    integration_points = {'target_detail_button': {'namespace': 'custom_code:vet',
+                                                   'title': 'Run kilonova candidate vetting',
+                                                   'class': "btn btn-pink",
+                                                   'text': 'Vet'}}
