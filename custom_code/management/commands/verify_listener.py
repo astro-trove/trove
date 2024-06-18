5ddf1324659fd718f54d3a4178164e8b61867c37
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2024-06-18 16:36:39 -0400

    check GraceDB to verify listener

--- custom_code/management/commands/verify_listener.py
+++ custom_code/management/commands/verify_listener.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from tom_nonlocalizedevents.models import EventSequence
+from tom_nonlocalizedevents.models import NonLocalizedEvent
 from django.conf import settings
 from datetime import datetime
 import requests
@@ -12,19 +12,24 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
-    help = 'Verifies that the tom-alertstreams listener is running by checking the time of the latest event'
+    help = 'Verifies that the tom-alertstreams listener is running by checking the latest event in GraceDB'
 
-    def add_arguments(self, parser):
-        parser.add_argument('--max-seconds', help='Longest acceptable time not to receive an alert in seconds',
-                            type=float, default=3600.)
-
-    def handle(self, max_seconds=3600., **kwargs):
-        es = EventSequence.objects.order_by('created').last()
-        now = datetime.now(es.created.tzinfo)
-        dt = (now - es.created).total_seconds()
-        message = f'The last GW alert was received {dt:.0f} s ago'
-        logger.info(message)
-        if dt > max_seconds:
+    def handle(self, **kwargs):
+        response = requests.get('https://gracedb.ligo.org/api/superevents')
+        response.raise_for_status()
+        json_data = response.json()
+        events = json_data['superevents']
+        latest_event = events[0]['superevent_id']
+        nle = NonLocalizedEvent.objects.filter(event_id=latest_event)
+        if nle.exists():
+            es = nle.last().sequences.last()
+            now = datetime.now(es.created.tzinfo)
+            dt = (now - es.created).total_seconds()
+            message = f'The last GW alert for {latest_event} was received {dt / 3600.:.1f} hours ago'
+            logger.info(message)
+        else:
+            message = f'The last GW alert for {latest_event} was not received'
+            logger.warning(message)
             headers = {'Content-Type': 'application/json'}
             data = json.dumps({'text': message}).encode('ascii')
             requests.post(settings.SLACK_URLS[0][-1], data=data, headers=headers)
