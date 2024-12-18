d09a547fed0740b7434a70f9dbc5dc1183ebfeb4
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2024-12-17 16:59:05 -0800

    add spectra table to end of spectra tab

--- custom_code/templatetags/spectroscopy_extras.py
+++ custom_code/templatetags/spectroscopy_extras.py
@@ -7,6 +7,7 @@ from guardian.shortcuts import get_objects_for_user
 from plotly import offline
 import plotly.graph_objs as go
 
+from tom_dataproducts.forms import DataShareForm
 from tom_dataproducts.models import DataProduct, ReducedDatum
 from tom_dataproducts.processors.data_serializers import SpectrumSerializer
 
@@ -61,4 +62,30 @@ def spectroscopy_for_target(context, target, dataproduct=None):
         'target': target,
         'products': datums,
         'plot': offline.plot(go.Figure(data=plot_data, layout=layout), output_type='div', show_link=False)
+    }
+
+
+# mostly copied from dataproduct_list_for_target in tom_base, but switched to ReducedDatum and filtered to only spectra
+@register.inclusion_tag('tom_dataproducts/partials/spectroscopy_datalist_for_target.html', takes_context=True)
+def get_spectroscopy_data(context, target):
+    """
+    Given a ``Target``, returns a list of ``DataProduct`` objects associated with that ``Target``
+    """
+    if settings.TARGET_PERMISSIONS_ONLY:
+        target_datums_for_user = target.reduceddatum_set.all()
+    else:
+        target_datums_for_user = get_objects_for_user(
+            context['request'].user, 'tom_dataproducts.view_reduceddatum', klass=target.reduceddatum_set.all())
+    spectroscopy_for_user = target_datums_for_user.filter(data_type='spectroscopy').order_by('timestamp')
+
+    initial = {'submitter': context['request'].user,
+               'target': target,
+               'share_title': f"Updated data for {target.name}."}
+    form = DataShareForm(initial=initial)
+
+    return {
+        'datums': spectroscopy_for_user,
+        'target': target,
+        'sharing_destinations': form.fields['share_destination'].choices,
+        'data_product_share_form': form
     }
