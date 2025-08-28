68fb72b8887f0cd07e3ebb7ea1724c8b3a74468d
Noah Franz <noahfranz13@gmail.com>
2025-08-28 12:12:31 -0700

    get the score details working

--- custom_code/templatetags/event_candidate_extras.py
+++ custom_code/templatetags/event_candidate_extras.py
@@ -3,6 +3,8 @@ Some functions for accessing the EventCandidate table inside a django template
 """
 from urllib.parse import urlparse
 from django import template
+from django.template.defaultfilters import linebreaks
+from django.utils.safestring import mark_safe
 from tom_nonlocalizedevents.models import EventCandidate, NonLocalizedEvent
 from trove_targets.models import Target
 
@@ -39,4 +41,27 @@ def display_score_details(target_id):
     for event_candidate in target.eventcandidate_set.all():
         score_details.append(event_candidate.scorefactor_set.all())
 
-    return score_details
+    res = {}
+    keymap = dict(
+        skymap_score = "2D Localization Score",
+        ps_score = "Point Source Score (1 or 0)",
+        host_distance_score = "3D Association Score"
+    )
+    for queryset in score_details:
+        for score_factor in queryset:
+            nle = score_factor.event_candidate.nonlocalizedevent
+            if nle not in res:
+                res[nle] = ""
+            if score_factor.key in keymap:
+                label = keymap[score_factor.key]
+            else:
+                label = score_factor.key
+            res[nle] += f"&emsp;{label}: {float(score_factor.value):.2f}\n"
+
+    out = ""
+    for key, s in res.items():
+        out += f"<h6>{key}</h6>"
+        out += s
+        out += "\n\n"
+            
+    return mark_safe(linebreaks(out))
