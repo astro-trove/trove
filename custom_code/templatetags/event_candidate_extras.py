0fbe47eb08be9386a5ab33f485add3db9eb8e1e7
Noah Franz <noahfranz13@gmail.com>
2025-09-22 09:54:46 -0700

    update formatting for score details

--- custom_code/templatetags/event_candidate_extras.py
+++ custom_code/templatetags/event_candidate_extras.py
@@ -1,6 +1,7 @@
 """
 Some functions for accessing the EventCandidate table inside a django template
 """
+from functools import partial
 from urllib.parse import urlparse
 from django import template
 from django.template.defaultfilters import linebreaks
@@ -29,6 +30,8 @@ def get_candidate_event_score(target_id):
     
     return out
 
+
+
 @register.simple_tag
 def display_score_details(target_id):
 
@@ -43,9 +46,12 @@ def display_score_details(target_id):
 
     res = {}
     keymap = dict(
-        skymap_score = "2D Localization Score",
-        ps_score = "Point Source Score (1 or 0)",
-        host_distance_score = "3D Association Score"
+        skymap_score = ("2D Localization Score", _float_format),
+        ps_score = ("Point Source Score (1 or 0)", _bool_format),
+        host_distance_score = ("3D Association Score", _float_format),
+        phot_peak_lum = ("Maximum Luminosity", partial(_sci_format, unit="erg/s")),
+        phot_peak_time = ("Time of Maximum Light Curve", partial(_float_format, unit="days")),
+        phot_decay_rate = ("Light Curve Slope (positive is brightening)", partial(_float_format, unit="mag/day"))
     )
     for queryset in score_details:
         for score_factor in queryset:
@@ -53,10 +59,11 @@ def display_score_details(target_id):
             if nle not in res:
                 res[nle] = ""
             if score_factor.key in keymap:
-                label = keymap[score_factor.key]
+                label, fmter = keymap[score_factor.key]
             else:
                 label = score_factor.key
-            res[nle] += f"&emsp;{label}: {float(score_factor.value):.2f}\n"
+                fmter = _float_format
+            res[nle] += f"&emsp;{label}: {fmter(float(score_factor.value))}\n"
 
     out = ""
     for key, s in res.items():
@@ -65,3 +72,15 @@ def display_score_details(target_id):
         out += "\n\n"
             
     return mark_safe(linebreaks(out))
+
+def _float_format(flt, unit=""):
+    return f"{flt:.2f} {unit}"
+
+def _sci_format(flt, unit=""):
+    prefactor, power = f"{flt:.2e}".split("e")
+    if power[0] == "+":
+        power = power[1:]
+    return f"{prefactor} x 10<sup>{power}</sup> {unit}"
+
+def _bool_format(flt):
+    return int(flt)
