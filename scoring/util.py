8db429565916e8eda5bbf3f910e35bfa43b5dc64
Noah Franz <noahfranz13@gmail.com>
2026-06-16 15:51:37 -0500

    migrate candidate vetting to scoring

--- scoring/util.py
+++ scoring/util.py
@@ -71,6 +71,22 @@ MPC_KEYS = [
 ]
 
 
+def update_score_factor(event_candidate, key, value):
+    ScoreFactor.objects.update_or_create(
+        event_candidate=event_candidate, key=key, defaults=dict(value=value)
+    )
+
+
+def delete_score_factor(event_candidate, key):
+    """This is basically only used since we are updating various scores
+    and may want to delete some, rather than update them, in the process"""
+    # first get any score factors that match this event candidate and key
+    matches = ScoreFactor.objects.filter(event_candidate=event_candidate, key=key)
+
+    if matches.count():
+        matches.delete()
+
+
 def _check_phot_val(val, param_ranges, param_range_key):
     val_max = max(param_ranges[param_range_key])
     val_min = min(param_ranges[param_range_key])
