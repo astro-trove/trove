fc4a6d5d70b9db2e8a5712a65832fe4ce651defd
Noah Franz <noahfranz13@gmail.com>
2026-06-16 22:57:05 -0500

    link to full documentation instead of just API docs in navbar

--- saguaro_tom/urls.py
+++ saguaro_tom/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
         include("tom_nonlocalizedevents.urls", namespace="nonlocalizedevents"),
     ),
     path("api/", api.urls),
-    path("docs/", include("sphinx_docs.urls")),
+    path("docs/", include("sphinx_docs.urls", namespace="sphinx_docs")),
 ]
