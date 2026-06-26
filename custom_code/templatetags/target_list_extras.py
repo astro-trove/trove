2deedffc4d069e2ea7e2d75921fa1113fed3d117
Shivam Panda <ompanda0910@gmail.com>
2026-06-25 20:50:40 -0500

    Added baseline changing of source name to proper label

--- custom_code/templatetags/target_list_extras.py
+++ custom_code/templatetags/target_list_extras.py
@@ -15,9 +15,52 @@ def galaxy_table(target):
     """
     Displays the most likely host galaxy matches.
     """
+
+    SOURCE_DISPLAY_NAMES = {
+        "GLADE": "GLADE",
+        "GladePlus": "GLADE+",
+
+        "DESI": "DESI",
+        "DesiDr1": "DESI DR1",
+        "DesiSpec": "DESI Spectroscopic Catalog",
+
+        "LsDr9North": "LS DR9 North",
+        "LsDr10": "LS DR10",
+        "LsDr10South": "LS DR10 South",
+        "LS_DR10": "LS DR10",
+
+        "Sdss12Photoz": "SDSS DR12 Photometric",
+        "SDSS_DR12": "SDSS DR12",
+
+        "Ps1Galaxy": "Pan-STARRS1 Galaxy Catalog",
+        "PS1_STRM": "Pan-STARRS1 STRM",
+
+        "NedLvs": "NED-LVS",
+
+        "GWGC": "GWGC",
+        "Gwgc": "GWGC",
+
+        "Cosmicflows4": "Cosmicflows-4",
+
+        "ExtendedVirgoClusterCatalog": "Extended Virgo Cluster Catalog (EVCC)",
+
+        "Hecate": "HECATE",
+        "HECATE": "HECATE",
+        "Hecate1": "HECATE",
+        "Hecate2": "HECATE",
+    }
+
     te = TargetExtra.objects.filter(target=target, key='Host Galaxies')
     if te.exists():
         galaxies = json.loads(te.first().value)
+        
+        try:
+            for galaxy in galaxies:
+                cur_source = galaxy['Source']
+                galaxy['Source'] = SOURCE_DISPLAY_NAMES[cur_source]
+        except:
+            print("Unable to change name")
+        
     else:
         galaxies = None
     return {'galaxies': galaxies}
