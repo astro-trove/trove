fedf3adee39f96673b86821378675783ac584f5e
nvieira-astro <nicholas.vieira@northwestern.edu>
2026-06-30 23:23:30 -0500

    Remove no longer necessary changes

--- custom_code/templatetags/target_list_extras.py
+++ custom_code/templatetags/target_list_extras.py
@@ -16,82 +16,32 @@ def galaxy_table(target):
     """
     Displays the most likely host galaxy matches.
     """
-
-    SOURCE_DISPLAY_NAMES = {
-        "GLADE": "GLADE+",
-        "GladePlus": "GLADE+",
-
-        "DESI": "DESI",
-        "DesiDr1": "DESI DR1",
-        "DesiSpec": "DESI Spectroscopic Catalog",
-
-        "LsDr9North": "LS DR9 North",
-        "LsDr10": "LS DR10",
-        "LsDr10South": "LS DR10 South",
-        "LS_DR10": "LS DR10",
-
-        "Sdss12Photoz": "SDSS DR12 Photometric",
-        "SDSS_DR12": "SDSS DR12",
-
-        "Ps1Galaxy": "Pan-STARRS1 Galaxy Catalog",
-        "PS1_STRM": "Pan-STARRS1 STRM",
-
-        "NedLvs": "NED-LVS",
-
-        "GWGC": "GWGC",
-        "Gwgc": "GWGC",
-
-        "Cosmicflows4": "Cosmicflows-4",
-
-        "ExtendedVirgoClusterCatalog": "Extended Virgo Cluster Catalog (EVCC)",
-
-        "Hecate": "HECATE",
-        "HECATE": "HECATE",
-        "Hecate1": "HECATE",
-        "Hecate2": "HECATE",
-    }
-
     te = TargetExtra.objects.filter(target=target, key='Host Galaxies')
     if te.exists():
         galaxies = json.loads(te.first().value)
-        
-        try:
-            for galaxy in galaxies:
+        for galaxy in galaxies:
+            z = galaxy['z']
+            z_err = galaxy['zErr']
 
-
-        
-    te = TargetExtra.objects.filter(target=target, key='Host Galaxies')
-    if te.exists():
-        try: 
-            galaxies = json.loads(te.first().value)
-            for galaxy in galaxies:
-                cur_source = galaxy['Source']
-                galaxy['Source'] = SOURCE_DISPLAY_NAMES[cur_source]
-              
-                z = galaxy['z']
-                z_err = galaxy['zErr']
-
+            try:
+                # If zErr is a float
+                places = -math.floor(math.log10(z_err))
+                galaxy['z'] = f"{z:.{places}f}"
+                galaxy['zErr'] = f'{z_err:.{places}f}'
+            except:
+                # If zErr is a list of upper and lower error
+                places_l = []
+                rounded_bounds = []
                 try:
-                    # If zErr is a float
-                    places = -math.floor(math.log10(z_err))
-                    galaxy['z'] = f"{z:.{places}f}"
-                    galaxy['zErr'] = f'{z_err:.{places}f}'
+                    for z_err_bound in z_err:
+                        place = -math.floor(math.log10(z_err_bound))
+                        places_l.append(place)
+                        rounded_bounds.append(f'{z_err_bound:.{place}f}')
+                    galaxy['z'] = f"{z:.{min(places_l)}f}"
+                    galaxy['zErr'] = rounded_bounds
                 except:
-                    # If zErr is a list of upper and lower error
-                    places_l = []
-                    rounded_bounds = []
-                    try:
-                        for z_err_bound in z_err:
-                            place = -math.floor(math.log10(z_err_bound))
-                            places_l.append(place)
-                            rounded_bounds.append(f'{z_err_bound:.{place}f}')
-                        galaxy['z'] = f"{z:.{min(places_l)}f}"
-                        galaxy['zErr'] = rounded_bounds
-                    except:
-                        # Considers case when NaN, does nothing
-                        pass
-        except:
-            print("Unable to change name")
+                    # Considers case when NaN, does nothing
+                    pass
 
     else:
         galaxies = None
